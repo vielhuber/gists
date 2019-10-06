a7e11ae9f6a685f9c0685f44f2faaeefa1cddc0b
David Vielhuber <david@vielhuber.de>
2019-10-06 02:34:52 +0200

    .

--- _cron.sh
+++ _cron.sh
@@ -3,5 +3,5 @@ cd /kunden/362380_80937/gists
 git pull
 env -i /usr/local/bin/php72 -f _sync.php
 git add -A .
-git commit --author="David Vielhuber <david@vielhuber.de>" -m "last update on `date +'%Y-%m-%d'`"
+git commit -m "last update on `date +'%Y-%m-%d'`"
 git push origin HEAD
