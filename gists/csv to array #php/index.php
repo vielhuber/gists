a0e67b2c58762f776eb11a1de512d436f41076f5
David Vielhuber <david@vielhuber.de>
2019-10-16 00:06:29 +0200

    last update on 2019-10-16

--- gists/csv to array #php/index.php
+++ gists/csv to array #php/index.php
@@ -4,5 +4,5 @@ $array = array_map(function($d) {
   return array_map(function($d2) {
     if( !mb_detect_encoding($d2, 'UTF-8', true) ) { $d2 = utf8_encode($d2); }
     return $d2;
-  }, str_getcsv($d, ';'));
+  }, str_getcsv($d, ';', '"'));
 }, file($csv));
