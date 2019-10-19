fdc1adac99560c5be3b52f2657d4b1cb018413bc
David Vielhuber <david@vielhuber.de>
2019-10-20 00:06:13 +0200

    last update on 2019-10-20

--- gists/Tricks für Laravel Eloquent Relationships #laravel/12.php
+++ gists/Tricks für Laravel Eloquent Relationships #laravel/12.php
@@ -5,8 +5,25 @@ class ConvenienceCollection extends Collection
 {
     public function __call($name, $args)
     {
-        return new ConvenienceCollection($this->map(function($item,$key) use ($name,$args) {
+        $collection = $this->map(function ($item, $key) use ($name, $args) {
             return $item->$name(...$args);
-        })->flatten()->unique()->filter(function ($value, $key) { return __x($value); }));
+        })->flatten()->unique()->filter(function ($value, $key) {
+            return __x($value);
+        });
+        /*
+        here it get's tricky:
+        the ConvenienceCollection is a Database\Eloquent\Collection (which extends Support\Collection)
+        because we want db functions like find() to work,
+        there is a point where we get only items in the collection which a no models anymore.
+        we detect this and break out of the convenience zone.
+        this makes things possible like: 
+        Person::find(42)->getAddresses()->getPerson()->getAddresses()->getPerson()->getId()->contains(42)
+        */
+        if (is_subclass_of($collection->first(), 'App\Model')) {
+            $collection = new ConvenienceCollection($collection);
+        } else {
+            $collection = collect($collection);
+        }
+        return $collection;
     }
 }
