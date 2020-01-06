533e26e231da6e10f6c2c9abddc8b9e9eda822cc
David Vielhuber <david@vielhuber.de>
2020-01-07 00:07:22 +0100

    last update on 2020-01-07

--- gists/disable autocomplete for all form inputs (with firefox back button bug) #js/script.js
+++ gists/disable autocomplete for all form inputs (with firefox back button bug) #js/script.js
@@ -4,6 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     	el.setAttribute('autocomplete', 'off');
     });
   }
+  document.addEventListener(
+    'focus',
+    e => {
+      if (e.target.closest('form')) {
+        if (!e.target.closest('form').hasAttribute('autocomplete')) {
+          e.target.closest('form').setAttribute('autocomplete', 'off');
+        }
+      }
+    },
+    true
+  );
 });
 // fix firefox bug (https://gist.github.com/vielhuber/4baf17fbf0e581ad351146323cfc36de)
 window.addEventListener('beforeunload', () => {
