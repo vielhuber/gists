c8efd6bb8a2f3a1b413c07758c44ad99bee16ed3
David Vielhuber <david@vielhuber.de>
2020-01-24 00:06:45 +0100

    last update on 2020-01-24

--- gists/PostgreSQL Full text search #sql/script.sql
+++ gists/PostgreSQL Full text search #sql/script.sql
@@ -4,5 +4,8 @@ SELECT * FROM persons WHERE to_tsvector('german', name) @@ plainto_tsquery('germ
 CREATE INDEX persons_idx ON persons USING gin(to_tsvector('german', (first_name || ' ' || last_name)));
 SELECT * FROM persons WHERE to_tsvector('german', (first_name || ' ' || last_name)) @@ plainto_tsquery('german', 'David Vielhuber');
 
-# this matches also!
+# you can also add wildcards to have a more mature search
 SELECT * FROM persons WHERE to_tsvector('german', (first_name || ' ' || last_name)) @@ to_tsquery('german', 'Dav:* & Vie:*');
+
+# remove 'german' for general word searching
+SELECT * FROM persons WHERE to_tsvector(first_name || ' ' || last_name) @@ to_tsquery('Dav:* & Vie:*');
