b6d18a0028d0da27aef082de04bcb6e078d57c5c
David Vielhuber <david@vielhuber.de>
2020-02-07 00:06:51 +0100

    last update on 2020-02-07

--- gists/program and submit own custom plugin php-scoper #wordpress/pluginname.php
+++ gists/program and submit own custom plugin php-scoper #wordpress/pluginname.php
@@ -18,11 +18,13 @@ register_activation_hook(__FILE__, function() {
 });
 
 
-// uninstall hook
+// uninstall hook (not recommended, does not work when deleting options, instead create an "uninstall.php" file)
+/*
 register_uninstall_hook(__FILE__, 'my_plugin_uninstall'); // warning: anonymous functions don't work here
 function my_plugin_uninstall() {
     delete_option('my_plugin_settings')
 }
+*/
 
 // localization
 // first create languages/my-plugin-de_DE.po/.mo
