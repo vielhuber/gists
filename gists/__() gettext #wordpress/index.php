d6083ff078e704701a7579861f75813138903690
David Vielhuber <david@vielhuber.de>
2020-02-09 00:06:38 +0100

    last update on 2020-02-09

---
+++ gists/__() gettext #wordpress/index.php
@@ -0,0 +1,5 @@
+<?php
+__('string', 'domain')
+__e('string', 'domain') // same as __(), but echoes
+_x('string', 'context', 'domain') // same as __(), but with context
+_ex('string', 'context', 'domain') // same as _x(), but echoes
