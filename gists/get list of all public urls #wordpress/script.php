a32db082bedd287b6fe9fa108e550dda04505058
David Vielhuber <david@vielhuber.de>
2020-02-23 00:15:19 +0100

    last update on 2020-02-23

--- gists/get list of all public urls #wordpress/script.php
+++ gists/get list of all public urls #wordpress/script.php
@@ -1,5 +1,17 @@
 $query = new \WP_Query(['post_type' => 'any', 'posts_per_page' => '-1', 'post_status' => 'publish']);
 while ($query->have_posts()) {
   $query->the_post();
-  echo get_the_permalink() . '<br/>';
+  $url = get_the_permalink();
+  echo $url . '<br/>';
+}
+$query = new \WP_Term_Query(['hide_empty' => false]);
+if (!empty($query->terms)) {
+  foreach ($query->terms as $terms__value) {
+    $url = get_term_link($terms__value);
+    // exclude non-public
+    if (strpos($url, '?') !== false) {
+      continue;
+    }
+    echo $url.'<br/>';
+  }
 }
