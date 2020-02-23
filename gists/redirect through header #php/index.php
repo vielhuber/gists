c1948f325a7765e4b6577402a87363bd066a7eee
David Vielhuber <david@vielhuber.de>
2020-02-24 00:14:59 +0100

    last update on 2020-02-24

--- gists/redirect through header #php/index.php
+++ gists/redirect through header #php/index.php
@@ -2,4 +2,5 @@
 header('Location: http://www.google.de');
 header('Location: http://www.google.de', true, 301); // Moved permanently
 header('Location: http://www.google.de', true, 302); // Moved temporarily
+header('Location: http://www.google.de', true, 307); // Temporary redirect: Always use this, if you want to redirect a POST request (so the next request must also be POST!)
 die();
