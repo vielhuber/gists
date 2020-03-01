10cf004d14982d1b87660fc4b1e3df9b52dd05ad
David Vielhuber <david@vielhuber.de>
2020-03-02 00:15:04 +0100

    last update on 2020-03-02

--- gists/post submit unchecked checkboxes #js/script.js
+++ gists/post submit unchecked checkboxes #js/script.js
@@ -1,13 +1,46 @@
-$('form').submit(function(e){
-    $('input:checkbox:not(:checked)[name]').not("input:checkbox[name$='[]']").not('[disabled="disabled"]').each(function() {
-        if( $(this).prev('input[type="hidden"][value="0"]').length === 0 ) {
-            $(this).before('<input type="hidden" value="0" name="'+$(this).attr('name')+'" />');
-        }
-    });
-    $("input:checkbox[name$='[]']").not('[disabled="disabled"]').each(function() {
-        if( $(document).find('input:checkbox[name="'+$(this).attr('name')+'"]:checked').length === 0 ) {
-            if( $(document).find("input[type='hidden'][name='"+$(this).attr('name')+"']").length === 0 ) {
-                $(this).before('<input type="hidden" value="" name="'+$(this).attr('name')+'" />');
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('submit', e => {
+        if (e.target.closest('form')) {
+            let form = e.target.closest('form'),
+                els = null;
+
+            // single
+            els = form.querySelectorAll(
+                'input[type="checkbox"]:not(:checked)[name]:not([name$=\'[]\']):not([disabled="disabled"])'
+            );
+            if (els.length > 0) {
+                els.forEach(el => {
+                    if (
+                        el.previousElementSibling === null ||
+                        el.previousElementSibling.getAttribute('type') !== 'hidden' ||
+                        el.previousElementSibling.value != '0'
+                    ) {
+                        el.insertAdjacentHTML(
+                            'beforebegin',
+                            '<input type="hidden" value="0" name="' + el.getAttribute('name') + '" />'
+                        );
+                    }
+                });
+            }
+
+            // multiple
+            els = form.querySelectorAll('input[type="checkbox"][name$=\'[]\']:not([disabled="disabled"])');
+            if (els.length > 0) {
+                els.forEach(el => {
+                    if (
+                        document.querySelector(
+                            'input[type="checkbox"][name="' + el.getAttribute('name') + '"]:checked'
+                        ) === null &&
+                        document.querySelector(
+                            'input[type="hidden"][name="' + el.getAttribute('name') + '"]'
+                        ) === null
+                    ) {
+                        el.insertAdjacentHTML(
+                            'beforebegin',
+                            '<input type="hidden" value="0" name="' + el.getAttribute('name') + '" />'
+                        );
+                    }
+                });
             }
         }
     });
