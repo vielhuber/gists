2c136690eb1d5b89eab5f8cdc9e5dbcc19f91d8f
David Vielhuber <david@vielhuber.de>
2020-04-01 00:07:35 +0200

    last update on 2020-04-01

--- gists/php 301 redirect mapping generator htaccess #php #server/index.php
+++ gists/php 301 redirect mapping generator htaccess #php #server/index.php
@@ -1,82 +1,91 @@
 <?php
-require_once 'spout/src/Spout/Autoloader/autoload.php';
-use Box\Spout\Reader\ReaderFactory;
-use Box\Spout\Common\Type;
+require_once __DIR__ . '/vendor/autoload.php';
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+
+$config = [
+    'input' => 'internal_html.xlsx',
+    'skip_first_line' => false,
+    'follow_redirects' => false,
+    'domains' => [
+        'old' => 'tld.de',
+        'new' => 'www.tld.de',
+        'test' => 'tld.dev'
+    ]
+];
 
 $rules = [];
-
-$reader = ReaderFactory::create(Type::XLSX);
-$reader->open('input.xlsx');
+$reader = ReaderEntityFactory::createXLSXReader();
+$reader->open($config['input']);
 foreach ($reader->getSheetIterator() as $sheets__key => $sheets__value) {
     foreach ($sheets__value->getRowIterator() as $rows__key => $rows__value) {
-        if ($rows__key === 1) {
+        $cells = $rows__value->toArray();
+        if ($config['skip_first_line'] === true && $rows__key === 1) {
             continue;
         }
-        if (@$rows__value[0] == '') {
+        if (@$cells[0] == '') {
             continue;
         }
-        $target = $rows__value[0];
-      	// if needed
-      	$target = get_redirect_final_target($target);
-        $target = str_replace(['http://test.tld.com/', 'https://test.tld.com/'], 'https://www.tld.com/', $target);
-        foreach ($rows__value as $rows__value__key => $rows__value__value) {
-            if ($rows__value__key === 0) {
-                continue;
+        $source = null;
+        $target = null;
+        $endless_loop = false;
+        foreach (['source', 'target'] as $type__key => $type__value) {
+            ${$type__value} = $cells[$type__key];
+            if ($config['follow_redirects'] === true) {
+                ${$type__value} = get_redirect_final_target(${$type__value});
             }
-            if ($rows__value__value !== '') {
-                $source = $rows__value__value;
-                if (strrpos($source, '/') === strlen($source)-1) {
-                    $source = substr($source, 0, strrpos($source, '/'));
+            foreach ($config['domains'] as $domains__value) {
+                foreach (['http://', 'http://www.', 'https://', 'https://www.'] as $protocol__value) {
+                    ${$type__value} = str_replace($protocol__value . $domains__value, '', ${$type__value});
                 }
-                // strip ?
-                if(strrpos($source, '?') !== false)
-                {
-                    $source = substr($source, 0, strrpos($source, '?'));
-                }
-                // prevent endless loops
-                $endless_loop_source = str_replace(['http://www.tld.com','https://www.tld.com'], '', $source);
-                $endless_loop_target = str_replace(['http://www.tld.com','https://www.tld.com'], '', $target);
-                if (strrpos($endless_loop_target, '/') === strlen($endless_loop_target)-1) {
-                    $endless_loop_target = substr($endless_loop_target, 0, strrpos($endless_loop_target, '/'));
-                }
-                if( $endless_loop_source === $endless_loop_target )
-                {
-                    continue;
-                }
-                // rel links
-                $source = str_replace(['http://www.tld.com/','https://www.tld.com/'], '', $source);
-              	// escape spaces
-              	if( strpos($source, ' ') !== false ) {
-                 	$source = str_replace(' ', '\ ', $source); 
-                }
-                $rules[] = [$source, $target];
             }
+            // strip "/"
+            ${$type__value} = trim(${$type__value}, '/');
+            // strip ?
+            if (strrpos(${$type__value}, '?') !== false) {
+                ${$type__value} = mb_substr(${$type__value}, 0, strrpos(${$type__value}, '?'));
+            }
+            // prevent endless loops
+            if ($type__value === 'target' && stripslashes($source) === ${$type__value}) {
+                $endless_loop = true;
+                break;
+            }
+            // escape
+            if ($type__value === 'source') {
+                foreach (
+                    ['\\', '.', '^', '$', '*', '+', '-', '?', '(', ')', '[', ']', '{', '}', '|', ' ']
+                    as $chars__value
+                ) {
+                    ${$type__value} = str_replace($chars__value, '\\' . $chars__value, ${$type__value});
+                }
+            }
+            // use abs url on target
+            if ($type__value === 'target') {
+                ${$type__value} = 'https://' . $config['domains']['new'] . '/' . ${$type__value};
+            }
+        }
+        if ($endless_loop === false) {
+            $rules[] = [$source, $target];
         }
     }
 }
 $reader->close();
 
-/*
-echo '<pre>';
-print_r($rules);
-*/
-
 $htaccess = '';
 $htaccess .= '<IfModule mod_rewrite.c>' . PHP_EOL;
 $htaccess .= 'RewriteEngine On' . PHP_EOL;
 foreach ($rules as $rules__value) {
-    $htaccess .=
-        'RewriteRule ^' . $rules__value[0] . '\/?$ ' . $rules__value[1] . ' [R=301,L]' . PHP_EOL;
+    $htaccess .= 'RewriteRule ^' . $rules__value[0] . '\/?$ ' . $rules__value[1] . ' [R=301,L]' . PHP_EOL;
 }
 $htaccess .= '</IfModule>';
 file_put_contents('.htaccess', $htaccess);
-die('ok');
+echo $htaccess;
+die();
 
 function get_redirect_final_target($url)
 {
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_USERPWD, 'username:password');
-    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    //curl_setopt($ch, CURLOPT_USERPWD, 'username:password');
+    //curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($ch, CURLOPT_NOBODY, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // follow redirects
     curl_setopt($ch, CURLOPT_AUTOREFERER, 1); // set referer on redirect
