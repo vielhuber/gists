3042d466812e1b37f3703d1948ecd363d77694b6
David Vielhuber <david@vielhuber.de>
2020-06-09 00:06:54 +0200

    last update on 2020-06-09

--- gists/sqlite #php/index.php
+++ gists/sqlite #php/index.php
@@ -9,4 +9,14 @@ $result = $query->fetchAll(\PDO::FETCH_ASSOC);
 $query = $db->prepare('UPDATE foo SET bar = ? WHERE id = ?');
 $query->execute(['test',42]);
 
+$db->exec('CREATE TABLE IF NOT EXISTS gnarr(
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    url VARCHAR(255),
+    url_orig VARCHAR(255) NOT NULL,
+    string TEXT,
+    context VARCHAR(10),
+    lng VARCHAR(10),
+    time FLOAT
+)');
+
 $db = null; // destroy
