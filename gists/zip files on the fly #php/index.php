319521ada7f9aec90c2481e8de83d91637a5a78b
David Vielhuber <david@vielhuber.de>
2020-06-13 00:09:58 +0200

    last update on 2020-06-13

--- gists/zip files on the fly #php/index.php
+++ gists/zip files on the fly #php/index.php
@@ -1,9 +1,10 @@
 $tmpfile = tempnam("tmp", "zip");
 $zip = new ZipArchive();
-$zip->open($tmpfile, ZipArchive::OVERWRITE);
+$zip->open($tmpfile, ZipArchive::CREATE | ZipArchive::OVERWRITE);
 
-foreach($files as $f) {
-  $zip->addFile(($_SERVER['DOCUMENT_ROOT'].'/.../'.$file->filename.'.'.$file->filetype), $file->name.'.'.$file->filetype);
+$files = [[$_SERVER['DOCUMENT_ROOT'].'/.../foo.txt','foo.txt'],[$_SERVER['DOCUMENT_ROOT'].'/.../bar.txt','bar.txt']];
+foreach($files as $files__value) {
+  $zip->addFile(($_SERVER['DOCUMENT_ROOT'].'/.../'.$files__value[0]), $files__value[1]);
 }
 
 $zip->close();
