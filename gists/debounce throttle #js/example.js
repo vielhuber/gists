1b877c736400b4a3b5985264b01beaeb9fd675a3
David Vielhuber <david@vielhuber.de>
2020-07-10 00:08:42 +0200

    last update on 2020-07-10

--- gists/debounce throttle #js/example.js
+++ gists/debounce throttle #js/example.js
@@ -7,3 +7,7 @@ document.querySelector('.container').addEventListener('input', hlp.debounce((e) 
 /* conditional debounce */
 let debounce = debounce(e => { console.log(e); }, 1000); // create this first (important to prevent wrong behaviour)
 document.querySelector('.container').addEventListener('input', e => { if (e.target && e.target.tagName === 'TEXTAREA') { debounce(e); } });
+
+/* manually call debounce */
+let fn = debounce(() => { console.log('debounce at resize') }, 1000);
+fn();
