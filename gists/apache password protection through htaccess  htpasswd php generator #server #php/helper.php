2f10a6d5cf6db7cbf4d7677a23e9c93dc3f21fe1
David Vielhuber <david@vielhuber.de>
2020-07-31 00:07:35 +0200

    last update on 2020-07-31

--- gists/apache password protection through htaccess  htpasswd php generator #server #php/helper.php
+++ gists/apache password protection through htaccess  htpasswd php generator #server #php/helper.php
@@ -2,13 +2,13 @@
 $username = '42';
 $password = '42';
 
-$content = "";
+$content = '';
 $content .= 'AuthName "Interner Bereich"'."\n";
 $content .= 'AuthType Basic'."\n";
 $content .= 'AuthUserFile '.str_replace('helper.php','.htpasswd',__file__)."\n";
 $content .= 'require valid-user'."\n\n";
 file_put_contents('.htaccess_new', $content);
 
-$content = "";
+$content = '';
 $content .= $username.':{SHA}'.base64_encode(sha1($password, true));
 file_put_contents('.htpasswd_new', $content);
