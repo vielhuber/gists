074fa3737fc5e95a80452918edc129c0ba1e019f
David Vielhuber <david@vielhuber.de>
2020-08-20 00:08:15 +0200

    last update on 2020-08-20

---
+++ gists/show usermeta fields in backend user profile and make editable #wordpress/functions.php
@@ -0,0 +1,54 @@
+<?php
+add_action('show_user_profile', function ($user) {
+    addFields($user);
+});
+add_action('edit_user_profile', function ($user) {
+    addFields($user);
+});
+add_action('personal_options_update', function ($user_id) {
+    saveFields($user_id);
+});
+add_action('edit_user_profile_update', function ($user_id) {
+    saveFields($user_id);
+});
+
+function addFields($user)
+{
+    echo '<h3>' . __('Meta fields', 'my-domain') . '</h3>';
+    echo '<table class="form-table">';
+    foreach ($this->getFields() as $fields__key => $fields__value) {
+        echo '<tr>';
+        echo '<th><label for="usermeta_' . $fields__key . '">' . $fields__value . '</label></th>';
+        echo '<td>';
+        echo '<input type="text" name="usermeta_' .
+            $fields__key .
+            '" id="usermeta_' .
+            $fields__key .
+            '" value="' .
+            esc_attr(get_user_meta($user->ID, $fields__key, true)) .
+            '" class="regular-text" />';
+        echo '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+}
+
+function saveFields($user_id)
+{
+    if (!current_user_can('edit_user', $user_id)) {
+        return false;
+    }
+    foreach ($this->getFields() as $fields__key => $fields__value) {
+        if (isset($_POST['usermeta_' . $fields__key])) {
+            update_user_meta($user_id, $fields__key, $_POST['usermeta_' . $fields__key]);
+        }
+    }
+}
+
+function getFields()
+{
+    return [
+        'foo' => __('Foo', 'my-domain'),
+        'bar' => __('Bar', 'my-domain')
+    ];
+}
