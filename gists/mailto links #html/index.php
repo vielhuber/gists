51e248c8ddf992a5fa61e1a6b916436238a40bf8
David Vielhuber <david@vielhuber.de>
2020-09-08 00:04:45 +0200

    last update on 2020-09-08

--- gists/mailto links #html/index.php
+++ gists/mailto links #html/index.php
@@ -2,9 +2,9 @@
 $to = ''; // leave empty if this should only open a new email
 
 $subject = '';
-$subject .= 'Test… M33 äöüßßß'; // must NOT be escaped!
+$subject .= 'Test… M33 äöüßßß'; // must be escaped (formerly, this must NOT be escaped due to an outlook bug, which is fixed now)
 
 $body = '';
 $body .= 'Sehen Sie sich folgenden Link an:'.PHP_EOL.PHP_EOL.'https://test.de'; // must be escaped
 
-echo '<a href="mailto:'.$to.'?subject='.$subject.'&amp;body='.rawurlencode($body).'">E-Mail senden</a>';
+echo '<a href="mailto:'.$to.'?subject='.rawurlencode($subject).'&amp;body='.rawurlencode($body).'">E-Mail senden</a>';
