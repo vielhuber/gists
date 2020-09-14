8f08041a8ca9601dc50841c2cb62f606972da9cf
David Vielhuber <david@vielhuber.de>
2020-09-15 00:04:57 +0200

    last update on 2020-09-15

--- gists/check trigger if all images are loaded (inside container, also background images included) #js/script.js
+++ gists/check trigger if all images are loaded (inside container, also background images included) #js/script.js
@@ -1,5 +1,6 @@
 /* npm install imagesloaded --save-dev */
 /* PROBLEM: dynamic changes are not triggered! */
+/* but this is intentionally: just use this function again after all dynamic changes are done! */
 imagesLoaded('.container', { background: true }, () =>
 {
   console.log('all images are loaded');
