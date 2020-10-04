ee7ac1d7573dbc7e63fd20ae6db9b44ba0a99d87
David Vielhuber <david@vielhuber.de>
2020-10-05 00:04:52 +0200

    last update on 2020-10-05

--- gists/disable frontend on testing #wordpress/functions.php
+++ gists/disable frontend on testing #wordpress/functions.php
@@ -3,7 +3,7 @@ if(
     strpos($_SERVER['HTTP_HOST'], 'close2dev') !== false &&
     !is_user_logged_in() &&
     !is_admin() &&
-    $pagenow != 'wp-login.php'
+    $GLOBALS['pagenow'] != 'wp-login.php'
 )
 {
     wp_redirect(get_admin_url());
