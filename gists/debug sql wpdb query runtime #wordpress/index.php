be58394935ee0a4968f8038d5784168a12628fb4
David Vielhuber <david@vielhuber.de>
2020-10-09 00:05:09 +0200

    last update on 2020-10-09

--- gists/debug sql wpdb query runtime #wordpress/index.php
+++ gists/debug sql wpdb query runtime #wordpress/index.php
@@ -2,13 +2,16 @@
 /* wp-config.php */
 define( 'SAVEQUERIES', true );
 
-/* footer.php */
-global $wpdb;
-print_r($wpdb->queries);
-// show slowest
-$queries = $wpdb->queries;
-usort($queries, function($a,$b) {
-    if ($a[1] == $b[1]) { return 0; }
-    return ($a[1] > $b[1]) ? -1 : 1;
-});
-print_r($queries[0][0]);
+/* functions.php */
+add_action('wp_footer', function($input) {
+  global $wpdb;
+  // show all
+  print_r($wpdb->queries);
+  // show slowest
+  $queries = $wpdb->queries;
+  usort($queries, function($a,$b) {
+      if ($a[1] == $b[1]) { return 0; }
+      return ($a[1] > $b[1]) ? -1 : 1;
+  });
+  print_r($queries[0][0]);
+}, PHP_INT_MAX);
