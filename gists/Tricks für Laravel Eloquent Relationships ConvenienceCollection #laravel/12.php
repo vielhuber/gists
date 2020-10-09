c8e16497326c65cbe02f859276f2f9d33efe3bdd
David Vielhuber <david@vielhuber.de>
2020-10-10 00:05:29 +0200

    last update on 2020-10-10

--- gists/Tricks für Laravel Eloquent Relationships ConvenienceCollection #laravel/12.php
+++ gists/Tricks für Laravel Eloquent Relationships ConvenienceCollection #laravel/12.php
@@ -5,6 +5,14 @@ class ConvenienceCollection extends Collection
 {
     public function __call($name, $args)
     {
+        /* the convenience collection has two modes:
+        - getFoo()
+            - if it contains a named attribute called "foo", it returns its value
+            - otherwise it applies getFoo() to all elements of the collection
+        */
+        if (strpos($name, 'get') === 0 && isset($this[snake_case(substr($name, 3))])) {
+            return $this[snake_case(substr($name, 3))];
+        }
         $collection = $this->map(function ($item, $key) use ($name, $args) {
             return $item->$name(...$args);
         })->flatten()->unique()->filter(function ($value, $key) {
@@ -14,7 +22,7 @@ class ConvenienceCollection extends Collection
         here it get's tricky:
         the ConvenienceCollection is a Database\Eloquent\Collection (which extends Support\Collection)
         because we want db functions like find() to work,
-        there is a point where we get only items in the collection which a no models anymore.
+        there is a point where we get only items in the collection which are no models anymore.
         we detect this and break out of the convenience zone.
         this makes things possible like: 
         Person::find(42)->getAddresses()->getPerson()->getAddresses()->getPerson()->getId()->contains(42)
