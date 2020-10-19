05f556faa8c01793b4d4ff60acba5cb9a7f27d49
David Vielhuber <david@vielhuber.de>
2020-10-20 00:05:28 +0200

    last update on 2020-10-20

--- gists/check  disable redirect access if user is on frontend  backend #wordpress/functions.php
+++ gists/check  disable redirect access if user is on frontend  backend #wordpress/functions.php
@@ -1,11 +1,11 @@
 <?php
 if( strpos($_SERVER['HTTP_HOST'], 'close2dev') !== false ) {
-  if( !is_admin() && $GLOBALS['pagenow'] != 'wp-login.php' ) {
+  if( !is_admin() && !in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php']) ) {
     // e.g. redirect to backend    
     wp_redirect(get_admin_url());
     die();
   }
-  if( is_admin() || $GLOBALS['pagenow'] == 'wp-login.php' ) {
+  else {
     die();
   }
 }
--- gists/disable frontend on testing #wordpress/functions.php
+++ gists/disable frontend on testing #wordpress/functions.php
@@ -3,7 +3,7 @@ if(
     strpos($_SERVER['HTTP_HOST'], 'close2dev') !== false &&
     !is_user_logged_in() &&
     !is_admin() &&
-    $GLOBALS['pagenow'] != 'wp-login.php'
+  	!in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php'])
 )
 {
     wp_redirect(get_admin_url());
