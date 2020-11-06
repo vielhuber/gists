4c18cf67b6265378399e65d9bab232674cc2bd22
David Vielhuber <david@vielhuber.de>
2020-11-07 00:05:23 +0100

    last update on 2020-11-07

--- gists/check  disable redirect access if user is on frontend  backend testing #wordpress/functions.php
+++ gists/check  disable redirect access if user is on frontend  backend testing #wordpress/functions.php
@@ -1,4 +1,5 @@
 <?php
+// disable backend on testing
 if( strpos($_SERVER['HTTP_HOST'], 'close2dev') !== false ) {
   if( !is_admin() && !in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php']) ) {
     // e.g. redirect to backend    
--- gists/get current language (with locale) #wpml #wordpress/script.php
+++ gists/get current language (with locale) #wpml #wordpress/script.php
@@ -1,6 +1,10 @@
 <?php
 // returns 'en'
-apply_filters( 'wpml_current_language', NULL )
+apply_filters( 'wpml_current_language', NULL );
+
+// returns 'en'
+global $sitepress;
+$lang = $sitepress->get_current_language();
 
 // returns 'en' (deprecated, does also not work when you switched languages programmatically)
 ICL_LANGUAGE_CODE
