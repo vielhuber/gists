1183d7f6aeda74bcbc6881d2b69d07d48a379d3f
David Vielhuber <david@vielhuber.de>
2021-01-01 00:04:45 +0100

    last update on 2021-01-01

--- gists/puppeteer js php fetch html code from page prerender (supports wsl) #js #php/index.js
+++ gists/puppeteer js php fetch html code from page prerender (supports wsl) #js #php/index.js
@@ -1,18 +1,28 @@
 const puppeteer = require('puppeteer');
 (async () => {
-    const browser = await puppeteer.launch({
-        args: [
-            '--disable-gpu',
-            '--disable-dev-shm-usage',
-            '--disable-setuid-sandbox',
-            '--no-first-run',
-            '--no-sandbox',
-            '--no-zygote',
-            '--single-process',
-        ],
-    });
-    const page = await browser.newPage();
-    await page.goto('https://vielhuber.de');
-    await page.screenshot({ path: 'example.png' });
-    await browser.close();
+  	try {
+      const browser = await puppeteer.launch({
+          headless: true,
+          ignoreHTTPSErrors: true,
+          args: [
+              '--disable-gpu',
+              '--disable-dev-shm-usage',
+              '--disable-setuid-sandbox',
+              '--no-first-run',
+              '--no-sandbox',
+              '--no-zygote',
+              '--single-process',
+          ],
+      });
+      const page = await browser.newPage();
+      await page.setDefaultTimeout(3000);
+      await page.goto('https://vielhuber.de', { waitUntil: 'networkidle2' });
+      await page.waitForSelector('.text');
+      let foo = await page.$eval('.text', (e) => e.innerHTML);
+      await page.screenshot({ path: 'example.png' });
+      await browser.close();
+    }
+  	catch (e) {
+     	console.log(e); 
+    }
 })();
