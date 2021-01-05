239007e1a1dd146598aeef289c5e61299b0dc1f5
David Vielhuber <david@vielhuber.de>
2021-01-06 00:04:58 +0100

    last update on 2021-01-06

--- gists/open socket for local tunnel domainfactory #sql #tools/bash.sh
+++ gists/open socket for local tunnel domainfactory #sql #tools/bash.sh
@@ -1,1 +1,1 @@
-ssh -TNL 5001:/var/lib/mysql/mysql.sock username@domain # then connect via localhost:5001 to mysql db
+ssh -TNL 5001:/var/lib/mysql/mysql.sock ssh-username@host # then connect via localhost:5001 to mysql db
