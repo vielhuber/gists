7a9e8675b8da5a9e9e829c6528f4ef7e50f2f335
David Vielhuber <david@vielhuber.de>
2021-01-14 00:05:09 +0100

    last update on 2021-01-14

--- gists/PHP-cli Show progress on command line through echo #php/index.php
+++ gists/PHP-cli Show progress on command line through echo #php/index.php
@@ -3,3 +3,10 @@ for ($i = 0; $i <= 100; $i++) {
     echo "Loading... {$i}%\r";
     usleep(10000);
 }
+
+for ($i = 100; $i >= 0; $i-) {
+    echo str_pad($i, 4, '0', STR_PAD_LEFT)."\r"; // every output should not be smaller than the previous one, so otherwise you get output formatting errors
+    usleep(10000);
+}
+
+
