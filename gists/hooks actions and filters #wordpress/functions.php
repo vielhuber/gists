e6ded380a6c6fc9e363e6762ce61a8085789a6f8
David Vielhuber <david@vielhuber.de>
2021-03-23 00:04:35 +0100

    last update on 2021-03-23

--- gists/hooks actions and filters #wordpress/functions.php
+++ gists/hooks actions and filters #wordpress/functions.php
@@ -8,3 +8,6 @@ add_filter('filter_name', function($input) {
 	// modify input and return
 	return $input;
 }, PHP_INT_MAX);
+add_filter('filter_name', function($input, $additional_input) {
+	return $input;
+}, PHP_INT_MAX, 2); // provide an argument count if you want to read more than 1 argument
