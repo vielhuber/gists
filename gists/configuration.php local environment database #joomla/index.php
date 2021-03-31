963b8190d29f83a0b196257411687cfc6246c26d
David Vielhuber <david@vielhuber.de>
2021-04-01 00:04:23 +0200

    last update on 2021-04-01

--- gists/configuration.php local environment database #joomla/index.php
+++ gists/configuration.php local environment database #joomla/index.php
@@ -1,25 +1,30 @@
 <?php
 class JConfig {
   ...
-	public $host = null;
-	public $user = null;
-	public $password = null;
-	public $db = null;
+    public $host = null;
+    public $user = null;
+    public $password = null;
+    public $db = null;
+    public $log_path = null;
+    public $tmp_path = null;
   ...
 
   public function __construct() {
-    if(in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', "::1"])) {
-  	//if( strpos($_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"], ".dev") === false ) {
-  		$this->host = "...";
-  		$this->user = "...";
-  		$this->password = "...";
-  		$this->db = "...";
-  	}
-  	else {
-  		$this->host = "...";
-  		$this->user = "...";
-  		$this->password = "...";
-  		$this->db = "...";
-  	}
+    if( @$_SERVER['SERVER_ADMIN'] === 'david@close2.de' || @$_SERVER['NAME'] === 'DAVID-DESKTOP' ) {
+        $this->host = '...';
+        $this->user = '...';
+        $this->password = '...';
+        $this->db = '...';
+        $this->log_path = __DIR__.'/logs';
+        $this->tmp_path = __DIR__.'/tmp';
+    }
+    else {
+        $this->host = '...';
+        $this->user = '...';
+        $this->password = '...';
+        $this->db = '...';
+        $this->password = '...';
+        $this->db = '...';
+    }
   }
 }
