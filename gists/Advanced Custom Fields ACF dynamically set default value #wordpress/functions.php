1fa4b83a48a59a64f7965e6135d5bb21352f9b99
David Vielhuber <david@vielhuber.de>
2021-04-30 00:05:26 +0200

    last update on 2021-04-30

--- gists/Advanced Custom Fields ACF dynamically set default value #wordpress/functions.php
+++ gists/Advanced Custom Fields ACF dynamically set default value #wordpress/functions.php
@@ -1,14 +1,19 @@
 <?php
 add_filter('acf/load_field/name=field_name', function($field) {
+     
+    // variant 1
+    $field['default_value'] = 'FOO';
+
+    // variant 2 (does not work anymore)
+    // use get_post_meta instead of get_field to avoid conflicts
     global $post;
-    
-    // variant 1 (use get_post_meta instead of get_field to avoid conflicts)
     if( get_post_meta($post->ID, 'field_name', true) == '' ) {
         $field['value'] = 'FOO';
     }
+
+    // if field should be disabled
+    $field['disabled'] = 1;
     
-    // variant 2
-    $field['default_value'] = 'FOO';
+    return $field; 
     
-    return $field;          
 });
