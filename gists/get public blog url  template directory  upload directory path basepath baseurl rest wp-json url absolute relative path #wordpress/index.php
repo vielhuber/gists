878d8f93988cbd8b75048acc4b0d010fefccb957
David Vielhuber <david@vielhuber.de>
2021-05-02 00:04:25 +0200

    last update on 2021-05-02

--- gists/get public blog url  template directory  upload directory path basepath baseurl rest wp-json url absolute relative path #wordpress/index.php
+++ gists/get public blog url  template directory  upload directory path basepath baseurl rest wp-json url absolute relative path #wordpress/index.php
@@ -1,5 +1,5 @@
 <?php
-// public url
+// public urls
 get_bloginfo('url')
 get_bloginfo('template_directory')
 get_stylesheet_directory_uri() // use this for child themes
@@ -8,15 +8,17 @@ admin_url( 'admin-ajax.php' ) // ajax url
 rest_url('v1/foo/bar') // rest url
 rest_url('v1/foo/bar?lang='.ICL_LANGUAGE_CODE) // rest url with wpml
 wp_lostpassword_url() // reset password url
+content_url() // wp-content
 
-// public url without language
+// public urls without language
 site_url()
    
-// full path
+// full paths
 get_home_path()
 get_template_directory()
 get_stylesheet_directory() // use this for child themes
 wp_upload_dir()['basedir']
+WP_CONTENT_DIR // wp-content
  
-// relative path
+// relative paths
 parse_url(get_template_directory_uri(), PHP_URL_PATH)
