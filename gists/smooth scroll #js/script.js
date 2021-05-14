7b8381ac5644194a1cdf07da4ae53b88d6a88793
David Vielhuber <david@vielhuber.de>
2021-05-15 00:05:17 +0200

    last update on 2021-05-15

--- gists/smooth scroll #js/script.js
+++ gists/smooth scroll #js/script.js
@@ -2,4 +2,4 @@ jQuery('html, body').animate({ scrollTop: 100 }, 1000); // needs jquery
 
 hlp.scrollTo( 100, 1000 ); // needs hlp
 
-window.scrollTo({ behavior: 'smooth', left: 0, top: 100 }); // does not work in IE
+window.scrollTo({ behavior: 'smooth', left: 0, top: 100 }); // does not work in IE or when smooth scrolling is disabled in chrome
