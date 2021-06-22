dde5ca6a78e4a750d55a95aca7da047f8aec3ca8
David Vielhuber <david@vielhuber.de>
2021-06-23 00:05:24 +0200

    last update on 2021-06-23

--- gists/native curl with wp_remote_get wp_remote_post #wordpress/functions.php
+++ gists/native curl with wp_remote_get wp_remote_post #wordpress/functions.php
@@ -1,3 +1,4 @@
+// GET
 $response = wp_remote_get('http://httpbin.org/get');
 if (is_wp_error($response)) {
     echo $response->get_error_message();
@@ -7,8 +8,10 @@ echo '<pre>';
 var_dump($response['headers'],json_decode($response['body']), $response['response']);
 echo '</pre>';
 
+// POST
 $response = wp_remote_post('http://httpbin.org/post', [
-    'body' => wp_json_encode([
+    'method' => 'POST',
+  	'body' => wp_json_encode([
         'foo' => 'bar',
         'bar' => 'baz'
     ]),
@@ -21,7 +24,6 @@ $response = wp_remote_post('http://httpbin.org/post', [
     'httpversion' => '1.0',
     'sslverify' => false,
     'data_format' => 'body',
-    'method' => 'POST',
     'cookies' => []
 ]);
 if (is_wp_error($response)) {
@@ -31,3 +33,9 @@ if (is_wp_error($response)) {
 echo '<pre>';    
 var_dump($response['headers'],json_decode($response['body']), $response['response']);
 echo '</pre>';
+
+// PUT
+$response = wp_remote_request('http://httpbin.org/post', [
+    'method' => 'PUT',
+    /* ... */
+]);
--- gists/remove a specific item from an array #js/script.js
+++ gists/remove a specific item from an array #js/script.js
@@ -1,1 +1,7 @@
-a = ["foo","bar"]; a.splice($.inArray("foo", a),1); // ["bar"]
+let a = ['foo', 'bar', null, 'baz'];
+
+delete a[1]; // ['foo', undefined, null, 'baz']
+
+a = a.filter(v => typeof v !== 'undefined'); // ['foo', null, 'baz']
+
+a = a.filter(v => v != 'baz'); // ['foo', null]
