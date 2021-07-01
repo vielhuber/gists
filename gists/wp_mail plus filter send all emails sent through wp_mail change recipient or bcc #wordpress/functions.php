1966ccdfe710547099e2e7d64ef2f78a84fef935
David Vielhuber <david@vielhuber.de>
2021-07-02 00:05:15 +0200

    last update on 2021-07-02

--- gists/wp_mail plus filter send all emails sent through wp_mail change recipient or bcc #wordpress/functions.php
+++ gists/wp_mail plus filter send all emails sent through wp_mail change recipient or bcc #wordpress/functions.php
@@ -1,7 +1,7 @@
 <?php
 // basic usage
 wp_mail(
-    'foo@bar.com',
+    'foo@bar.com', // use ['foo@bar.com','bar@baz.com'] for multiple recipients
     'The subject',
     'The <strong>html</strong> content',
     ['Content-Type: text/html; charset=UTF-8'],
