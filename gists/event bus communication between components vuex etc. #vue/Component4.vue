a8281511d5b2bc30062afa90270761dbc4beac37
David Vielhuber <david@vielhuber.de>
2021-07-08 00:04:41 +0200

    last update on 2021-07-08

--- gists/event bus communication between components vuex etc. #vue/Component4.vue
+++ gists/event bus communication between components vuex etc. #vue/Component4.vue
@@ -3,7 +3,7 @@
   import { EventBus } from '@/helpers/event-bus';
   mounted() {
     EventBus.$on('custom-event', data => {});
-  }
+  },
   beforeDestroy() {
     // this is very important: unsubscribe from the event when destroying (because otherwise the next time you double the triggers!)
     EventBus.$off('custom-event');
