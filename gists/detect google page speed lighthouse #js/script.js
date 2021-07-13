911b9d6a7b85f03f9dd00a4d64750101683666e8
David Vielhuber <david@vielhuber.de>
2021-07-14 00:05:00 +0200

    last update on 2021-07-14

---
+++ gists/detect google page speed lighthouse #js/script.js
@@ -0,0 +1,5 @@
+// working
+if (navigator.userAgent.indexOf('Speed Insights') > -1 || navigator.userAgent.indexOf('Chrome-Lighthouse') > -1) { }
+
+// deprecated
+if (navigator.language === 'en-US' && navigator.platform.indexOf('Linux') > -1) { }
