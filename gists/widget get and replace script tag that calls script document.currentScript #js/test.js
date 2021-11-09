5df8a78972777eaed052bad282ed1eb603f33550
David Vielhuber <david@vielhuber.de>
2021-11-10 00:05:29 +0100

    last update on 2021-11-10

--- gists/widget get and replace script tag that calls script document.currentScript #js/test.js
+++
@@ -1,8 +0,0 @@
-let script = document.currentScript;
-// in case the script is added dynamically, use fallback
-if (script.getAttribute('data-foo') === null) {
-    let scripts = document.getElementsByTagName('script');
-    script = scripts[scripts.length - 1];
-}
-
-script.outerHTML = 'replace with magic';
