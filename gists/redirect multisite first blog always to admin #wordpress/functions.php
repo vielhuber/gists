730e1eb09f9ac53e98550821a17643421a148262
David Vielhuber <david@vielhuber.de>
2022-01-06 00:05:37 +0100

    last update on 2022-01-06

--- gists/redirect multisite first blog always to admin #wordpress/functions.php
+++ gists/redirect multisite first blog always to admin #wordpress/functions.php
@@ -4,7 +4,8 @@ if (
     !is_admin() &&
     !in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php']) &&
     !(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') &&
-    !wp_is_json_request()
+    !wp_is_json_request() &&
+    !defined('DOING_CRON')
 ) {
     wp_redirect(get_admin_url());
     die();
