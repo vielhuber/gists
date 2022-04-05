b2afe548396ac1a36004c4f02a9d9e3754e5e341
David Vielhuber <david@vielhuber.de>
2022-04-06 00:05:54 +0200

    last update on 2022-04-06

--- gists/enable error reporting inline temporarly #php/index.php
+++ gists/enable error reporting inline temporarly #php/index.php
@@ -1,2 +1,12 @@
+<?php
+// full error reporting
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+
+// no error reporting
+error_reporting(0);
+
+// temporarily hide error reporting
+$previous = error_reporting(0);
+something_which_produces_warnings_which_can_safely_be_ignored();
+error_reporting($previous);
