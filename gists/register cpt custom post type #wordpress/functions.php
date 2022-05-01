c798d725294533140a169cb5f78996dcfb85a090
David Vielhuber <david@vielhuber.de>
2022-05-02 00:05:02 +0200

    last update on 2022-05-02

--- gists/register cpt custom post type #wordpress/functions.php
+++ gists/register cpt custom post type #wordpress/functions.php
@@ -1,26 +1,35 @@
+<?php
 add_action('init', function () {
-    register_post_type('book', [
+    $data = ['key' => 'book', 'slug' => 'books', 'label_singular' => 'Buch', 'label_plural' => 'Bücher'];
+    $args = [
         'labels' => [
-            'name' => 'Bücher',
-            'singular_name' => 'Buch',
-            'menu_name' => 'Bücher',
-            'name_admin_bar' => 'Buch',
-            'new_item' => 'Neues Buch',
-            'edit_item' => 'Buch editieren',
-            'view_item' => 'Buch ansehen',
-            'all_items' => 'Alle Bücher',
-            'not_found' => 'Keine Bücher gefunden'
+            'name' => $data['label_plural'],
+            'singular_name' => $data['label_singular'],
+            'menu_name' => $data['label_plural'],
+            'name_admin_bar' => $data['label_singular'],
+            'new_item' => 'Neues ' . $data['label_singular'],
+            'edit_item' => $data['label_singular'] . ' editieren',
+            'view_item' => $data['label_singular'] . ' ansehen',
+            'all_items' => 'Alle ' . $data['label_plural'],
+            'not_found' => 'Keine ' . $data['label_plural'] . ' gefunden'
         ],
-        'public' => true,
-        'publicly_queryable' => true,
         'show_ui' => true,
         'show_in_menu' => true,
         'query_var' => true,
-        'rewrite' => ['slug' => 'book'],
         'capability_type' => 'post',
         'has_archive' => true,
         'hierarchical' => false,
         'menu_position' => null,
         'supports' => ['title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments']
-    ]);
+    ];
+    if (isset($data['slug']) && $data['slug'] != '') {
+        $args['public'] = true;
+        $args['publicly_queryable'] = true;
+        $args['rewrite'] = ['slug' => $data['slug']];
+    } else {
+        $args['public'] = false;
+        $args['publicly_queryable'] = false;
+        $args['rewrite'] = null;
+    }
+    register_post_type($data['key'], $args);
 });
