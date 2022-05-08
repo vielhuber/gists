ed0fd1fb7f7fa87c2684a9091c13acbda968c387
David Vielhuber <david@vielhuber.de>
2022-05-09 00:04:55 +0200

    last update on 2022-05-09

--- gists/open new popup with window.open #js/script.js
+++ gists/open new popup with window.open #js/script.js
@@ -1,11 +1,15 @@
+let w = window.innerWidth/1.5,
+    h = window.innerHeight/1.5;
+if( w < 870 ) { w = 870; }
+if( h < 800 ) { h = 800; }
 let args = {
    url: 'https://tld.com',
    target: '_blank',
    specs: {
-      width: Math.round(window.innerWidth/1.5),
-      height: Math.round(window.innerHeight/1.5),
-      left: Math.round((window.innerWidth-(window.innerWidth/1.5))/2),
-      top: Math.round((window.innerHeight-(window.innerHeight/1.5))/1.4), // little bit shifted from top
+      width: Math.round(w),
+      height: Math.round(h),
+      left: Math.round((window.innerWidth-(w))/2),
+      top: Math.round((window.innerHeight-(h))/1.4), // little bit shifted from top
       menubar: false,
       scrollbars: false,
       status: false,
