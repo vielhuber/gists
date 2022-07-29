32636fc4d43632d62559acc67dfd832c9585ea6b
David Vielhuber <david@vielhuber.de>
2022-07-30 00:05:38 +0200

    last update on 2022-07-30

--- gists/send raw text html mail #laravel #php/MailController.php
+++ gists/send raw text html mail #laravel #php/MailController.php
@@ -12,6 +12,8 @@ public function send(Request $request)
       'to_name' => 'David Vielhuber',
       'reply_email' => 'david@vielhuber.de',
       'reply_name' => 'David Vielhuber',
+      'bcc_email' => 'david@vielhuber.de',
+      'bcc_name' => 'David Vielhuber',
       'attachment_filename' => 'path/to/file.pdf',
       'attachment_filename_public' => 'YourFile.pdf',
       'attachment_mime' => 'application/pdf'
@@ -24,19 +26,29 @@ public function send(Request $request)
       $message->from($data['from_email'], $data['from_name']);
       $message->to($data['to_email'], $data['to_name']);
       $message->replyTo($data['reply_email'], $data['reply_name']);
-      $message->subject($data['subject']);
+      $message->bcc($data['bcc_email'], $data['bcc_name']);
+      $message->subject($data['subject']);      
       $message->attach($data['attachment_filename'], ['as' => $data['attachment_filename_public'], 'mime' => $data['attachment_mime']]);
-    });
-    
-    // html
+      
+    });    
+    // text only
     Mail::send([], [], function($message) use ($data)
     {
-      $message->from($data['from_email'], $data['from_name']);
-      $message->to($data['to_email'], $data['to_name']);
-      $message->replyTo($data['reply_email'], $data['reply_name']);
-      $message->subject($data['subject']);
+      /* ... */
+      $message->setBody($data['message'], 'text/plain');
+    });
+    // html (plain text automatically)
+    Mail::send([], [], function($message) use ($data)
+    {
+	  /* ... */
       $message->setBody($data['message'], 'text/html');
-      $message->attach($data['attachment_filename'], ['as' => $data['attachment_filename_public'], 'mime' => $data['attachment_mime']]);
+    });
+    // html + plain (custom)
+    Mail::send([], [], function($message) use ($data)
+    {
+      /* ... */
+      $message->setBody($data['message'], 'text/html');
+      $message->addPart(strip_tags($data['message']), 'text/plain');
     });
     
     return response()->json(['success' => true], 200);
