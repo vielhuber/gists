f30d4a45c9dd2bb0a2fd5eb137de7be97aee3745
David Vielhuber <david@vielhuber.de>
2022-10-28 00:06:54 +0200

    last update on 2022-10-28

--- gists/imap #php/1.php
+++ gists/imap #php/1.php
@@ -1,7 +1,12 @@
+<?php
 // composer require php-imap/php-imap
-require_once(__DIR__ . '/vendor/autoload.php');
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpImap\Mailbox;
+
 $settings = [
-    'host' => 'outlook.office365.com',
+    'host' => '',
     'port' => 993,
     'username' => '***',
     'password' => '***',
@@ -10,13 +15,8 @@ $settings = [
 
 $mails = [];
 try {
-    $mailbox = new \PhpImap\Mailbox(
-        '{' .
-            $settings['host'] .
-            ':' .
-            $settings['port'] .
-            '/imap/ssl}' .
-            $settings['folder_inbox'],
+    $mailbox = new Mailbox(
+        '{' . $settings['host'] . ':' . $settings['port'] . '/imap/ssl}' . $settings['folder_inbox'],
         $settings['username'],
         $settings['password'],
         sys_get_temp_dir(),
@@ -25,14 +25,16 @@ try {
     $mails_ids = $mailbox->searchMailbox('ALL');
     foreach ($mails_ids as $mails_id__value) {
         $mail = $mailbox->getMail($mails_id__value, false); // don't mark as unread
-		$mail->embedImageAttachments();
 
+        // save as eml
+        $mail->embedImageAttachments();
         $eml_filename = tempnam(sys_get_temp_dir(), 'mail_') . '.eml';
         $unseen = in_array($mails_id__value, $mailbox->searchMailbox('UNSEEN', true));
         $mailbox->saveMail($mails_id__value, $eml_filename);
-        // undo saveMail setting mail as read
-        if ($unseen) { $mailbox->markMailAsUnread($mail_id); }     
-      
+        if ($unseen) {
+            $mailbox->markMailAsUnread($mail_id); // undo saveMail setting mail as read
+        }
+
         $mails[] = [
             'id' => (string) $mail->id,
             'mailbox' => $settings['username'],
@@ -41,7 +43,7 @@ try {
             'to' => (string) $mail->toString,
             'date' => $mail->date,
             'subject' => (string) $mail->subject,
-          	'eml' => base64_encode(file_get_contents($eml_filename)),
+            'eml' => base64_encode(file_get_contents($eml_filename)),
             'attachments' => count($mail->getAttachments()),
             'content_html' =>
                 mb_detect_encoding($mail->textHtml, 'UTF-8, ISO-8859-1') !== 'UTF-8'
@@ -54,6 +56,6 @@ try {
         ];
     }
 } catch (\Exception $e) {
-    continue;
+    print_r($e->getMessage());
 }
 print_r($mails);
---
+++ gists/imap #php/2.php
@@ -0,0 +1,93 @@
+<?php
+// composer require webklex/php-imap
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Webklex\PHPIMAP\ClientManager;
+
+$settings = [
+    'client_id' => '***',
+    'client_secret' => '***',
+    'tenant_id' => '***',
+    'email' => '***'
+];
+
+$ch = curl_init();
+try {
+    curl_setopt($ch, CURLOPT_URL, 'https://login.microsoftonline.com/' . $settings['tenant_id'] . '/oauth2/v2.0/token');
+    curl_setopt(
+        $ch,
+        CURLOPT_POSTFIELDS,
+        http_build_query([
+            'client_id' => $settings['client_id'],
+            'client_secret' => $settings['client_secret'],
+            'scope' => 'https://outlook.office365.com/.default',
+            'grant_type' => 'client_credentials'
+        ])
+    );
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $curl_result = curl_exec($ch);
+    if (empty($curl_result)) {
+        throw new Exception('Missing results.');
+    }
+    $curl_result = json_decode($curl_result);
+    if (empty($curl_result)) {
+        throw new Exception('Error decoding json result.');
+    }
+    if (!isset($curl_result->access_token)) {
+        throw new Exception('Missing access token from result.');
+    }
+    $access_token = $curl_result->access_token;
+} finally {
+    curl_close($ch);
+}
+
+$mails = [];
+$cm = new ClientManager();
+$client = $cm->make([
+    'host' => 'outlook.office365.com',
+    'port' => 993,
+    'encryption' => 'tls',
+    'validate_cert' => true,
+    'username' => $settings['email'],
+    'password' => $access_token,
+    'protocol' => 'imap',
+    'authentication' => 'oauth'
+]);
+$client->connect();
+$folders = $client->getFolders();
+foreach ($folders as $folder) {
+    if ($folder->full_name !== 'INBOX') {
+        continue;
+    }
+    $messages = $folder
+        ->messages()
+        ->all()
+        ->get();
+    foreach ($messages as $messages__value) {
+        $mail = [];
+        $mail['id'] = $messages__value->getMessageId()[0];
+        $mail['from_name'] = $messages__value->getFrom()[0]->personal;
+        $mail['from_email'] = $messages__value->getFrom()[0]->mail;
+        $mail['to'] = $messages__value->getTo()[0]->mail;
+        $mail['date'] = $messages__value
+            ->getDate()
+            ->toDate()
+            ->format('Y-m-d H:i:s');
+        $mail['subject'] = $messages__value->getSubject()[0];
+        $mail['eml'] =
+            json_decode(json_encode($messages__value->getHeader()), true)['raw'] . $messages__value->getRawBody();
+        $mail['attachments'] = $messages__value->getAttachments()->count();
+        $mail['content_html'] = $messages__value->getHTMLBody();
+        $mail['content_plain'] = $messages__value->getTextBody();
+
+        // methods
+        //$messages__value->setFlag('Seen');
+        //$messages__value->unsetFlag('Seen');
+        //$messages__value->move('INBOX/ARCHIV');
+
+        $mails[] = $mail;
+    }
+}
+print_r($mails);
