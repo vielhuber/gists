ee2ca32fe6f50beb079c61e6a5725c7028e654c2
David Vielhuber <david@vielhuber.de>
2022-12-20 00:06:02 +0100

    last update on 2022-12-20

--- gists/fetch all sitemap links #tools/bash.sh
+++
@@ -1,4 +0,0 @@
-curl --silent some-sitemap.xml | htmlq --text loc >> links.txt
-curl --silent some-other-sitemap.xml | htmlq --text loc >> links.txt
-split -l 300 links.txt links_
-wget -i links_aa -O /dev/null
