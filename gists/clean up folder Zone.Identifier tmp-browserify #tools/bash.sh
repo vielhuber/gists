fe86cfcd52fcde2d3d289c8fdec95d71acecf8e1
David Vielhuber <david@vielhuber.de>
2023-07-21 00:05:46 +0200

    last update on 2023-07-21

---
+++ gists/clean up folder Zone.Identifier tmp-browserify #tools/bash.sh
@@ -0,0 +1,4 @@
+find . -name "*:Zone.Identifier" -type f -delete
+find . -name "*.tmp-browserify-*" -type f -delete
+find . -name "desktop.ini" -type f -delete
+find . -name ".DS_Store" -type f -delete
