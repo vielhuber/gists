3a4fe58ad226129ae8782f9844e786bcccb38b8f
David Vielhuber <david@vielhuber.de>
2023-09-18 00:05:25 +0200

    last update on 2023-09-18

--- gists/social sharing share links on all major networks #php/index.php
+++ gists/social sharing share links on all major networks #php/index.php
@@ -4,13 +4,13 @@ $url = 'http'.((isset($_SERVER['HTTPS'])&&$_SERVER['HTTPS']!=='off')?'s':'').':/
 $title = 'David Vielhuber';
 $image = 'https://vielhuber.de/wp-content/themes/vielhuber/_assets/about.jpg';
 echo '<ul>';
-    echo '<li><a class="facebook" target="_blank" rel="nofollow" href="https://www.facebook.com/sharer/sharer.php?u='.urlencode($url).'">Facebook</a></li>';
-    echo '<li><a class="xing" target="_blank" rel="nofollow" href="https://www.xing.com/app/user?op=share&amp;url='.urlencode($url).'">Xing</a></li>';
-    echo '<li><a class="linkedin" target="_blank" rel="nofollow" href="https://www.linkedin.com/shareArticle?mini=true&url='.urlencode($url).'&title='.urlencode($title).'&summary=&source=">LinkedIn</a></li>';
-    echo '<li><a class="twitter" target="_blank" rel="nofollow" href="https://twitter.com/intent/tweet?url=&text='.urlencode($title.' - '.$url).'">Twitter</a></li>';
+    echo '<li><a class="facebook" target="_blank" rel="nofollow" href="https://www.facebook.com/sharer/sharer.php?u='.urlencode($url).'" title="Facebook">Facebook</a></li>';
+    echo '<li><a class="xing" target="_blank" rel="nofollow" href="https://www.xing.com/app/user?op=share&amp;url='.urlencode($url).'" title="Xing">Xing</a></li>';
+    echo '<li><a class="linkedin" target="_blank" rel="nofollow" href="https://www.linkedin.com/shareArticle?mini=true&url='.urlencode($url).'&title='.urlencode($title).'&summary=&source=" title="LinkedIn">LinkedIn</a></li>';
+    echo '<li><a class="twitter" target="_blank" rel="nofollow" href="https://twitter.com/intent/tweet?url=&text='.urlencode($title.' - '.$url).'" title="X (Twitter)">X (Twitter)</a></li>';
     if($image !== null ) {
-        echo '<li><a class="pinterest" target="_blank" rel="nofollow" href="https://www.pinterest.de/pin/create/button/?url='.urlencode($url).'&media='.urlencode($image).'&description='.urlencode($title).'">Pinterest</a></li>';
+        echo '<li><a class="pinterest" target="_blank" rel="nofollow" href="https://www.pinterest.de/pin/create/button/?url='.urlencode($url).'&media='.urlencode($image).'&description='.urlencode($title).'" title="Pinterest">Pinterest</a></li>';
     }
-    echo '<li><a class="whatsapp" rel="nofollow" href="whatsapp://send?text='.urlencode($title.' - '.$url).'">WhatsApp</a></li>';
-    echo '<li><a class="mail" rel="nofollow" href="mailto:?subject='.rawurlencode($title).'&body='.rawurlencode($url).'">E-Mail</a></li>';
+    echo '<li><a class="whatsapp" rel="nofollow" href="whatsapp://send?text='.urlencode($title.' - '.$url).'" title="WhatsApp">WhatsApp</a></li>';
+    echo '<li><a class="mail" rel="nofollow" href="mailto:?subject='.rawurlencode($title).'&body='.rawurlencode($url).'" title="E-Mail">E-Mail</a></li>';
 echo '</ul>';
