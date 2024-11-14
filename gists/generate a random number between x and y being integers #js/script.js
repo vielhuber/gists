4c85c0049f22bba8043101e7b1b3e943ebb7999c
David Vielhuber <david@vielhuber.de>
2024-11-15 00:05:59 +0100

    last update on 2024-11-15

--- gists/generate a random number between x and y being integers #js/script.js
+++ gists/generate a random number between x and y being integers #js/script.js
@@ -1,1 +1,7 @@
 ~~(Math.random()*(y-x+1))+x
+
+Explanation:
+- Math.random(): [0, 1)
+- Math.random()*(y-x+1): [0, y-x+1)
+- (Math.random()*(y-x+1))+x: [x, y+1)                      
+- ~~(Math.random()*(y-x+1))+x: [x, y]
