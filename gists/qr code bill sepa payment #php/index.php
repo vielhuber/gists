d0873aa3e95ed0363df45a2c1a7b7f2f7ef3370e
David Vielhuber <david@vielhuber.de>
2024-12-03 00:05:58 +0100

    last update on 2024-12-03

--- gists/qr code bill sepa payment #php/index.php
+++ gists/qr code bill sepa payment #php/index.php
@@ -4,34 +4,45 @@ composer require smhg/sepa-qr-data
 composer require endroid/qr-code
 */
 require_once(__DIR__ . '/vendor/autoload.php');
+
 use SepaQr\Data;
-use Endroid\QrCode\Builder\Builder;
-use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelMedium;
-use Endroid\QrCode\Label\Alignment\LabelAlignmentCenter;
-use Endroid\QrCode\Label\Font\NotoSans;
-use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
+
 use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Writer\SvgWriter;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Label\Font\OpenSans;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
 
-$filename = 'qr.png';
+$builder = new Builder(
+    writer: new PngWriter(),
+    data: Data::create()->setName('Max Mustermann')->setIban('DE42424242424242424242')->setAmount(1337.42),
+    size: 300,
+    margin: 10,
+    errorCorrectionLevel: ErrorCorrectionLevel::Medium,
+    roundBlockSizeMode: RoundBlockSizeMode::None,
+    labelText: 'Jetzt mit QR-Code bezahlen',
+    labelFont: new OpenSans(16),
+    labelAlignment: LabelAlignment::Center,
+    foregroundColor: new Color(89, 183, 130),
+    backgroundColor: new Color(255, 255, 255)
 
-Builder::create()
-    ->data(Data::create()
-        ->setName('Max Mustermann')
-        ->setIban('DE42424242424242424242')
-        ->setAmount(1337.42)
-    )
-    ->errorCorrectionLevel(new ErrorCorrectionLevelMedium())
-    ->size(300)
-    ->margin(10)
-    ->roundBlockSizeMode(new RoundBlockSizeModeMargin())
-    ->labelText('Jetzt mit QR-Code bezahlen')
-    ->labelFont(new NotoSans(16))
-    ->labelAlignment(new LabelAlignmentCenter())
-    ->foregroundColor(new Color(89, 183, 130))
-    ->backgroundColor(new Color(255, 255, 255))
-    ->build()
-    ->saveToFile($filename);
+    //writer: new SvgWriter(),
+    //logoPath: __DIR__ . '/../_assets/logo.svg',
+    //logoResizeToWidth: 50,
+    //logoResizeToHeight: 50,
+    //logoPunchoutBackground: true,
+);
+$result = $builder->build();
 
+// output
+echo '<img src="'.$result->getDataUri().'" alt="" />';
+
+// save
+$result->build()->saveToFile('qr.png');
 header('Content-Type: image/png');
 header('Content-Length: ' . filesize($filename));
 readfile($filename);
