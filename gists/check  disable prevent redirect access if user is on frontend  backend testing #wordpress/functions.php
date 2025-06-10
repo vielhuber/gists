2f32a9803b17010ce5f0433f9a6d7f7f5d4f5c77
David Vielhuber <david@vielhuber.de>
2025-06-11 00:05:57 +0200

    last update on 2025-06-11

--- gists/check  disable prevent redirect access if user is on frontend  backend testing #wordpress/functions.php
+++ gists/check  disable prevent redirect access if user is on frontend  backend testing #wordpress/functions.php
@@ -1,8 +1,8 @@
 <?php
 // disable frontend/backend on testing
-if( strpos($_SERVER['HTTP_HOST'], '.dev') !== false ) {
+if( !wp_doing_cron() && strpos(@$_SERVER['HTTP_HOST'], '.dev') !== false ) {
   // frontend redirect to backend
-  if( !is_admin() && !in_array($GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php']) ) { 
+  if( !is_admin() && !in_array(@$GLOBALS['pagenow'], ['wp-login.php', 'wp-register.php']) ) { 
     wp_redirect(get_admin_url());
     die();
   }
