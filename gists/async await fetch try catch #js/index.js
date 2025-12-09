925bd9b46a73099f38b4ba21ab6d8ed5db18e470
David Vielhuber <david@vielhuber.de>
2025-12-10 00:07:13 +0100

    last update on 2025-12-10

--- gists/async await fetch try catch #js/index.js
+++ gists/async await fetch try catch #js/index.js
@@ -1,36 +1,35 @@
 /* fetch returns a promise which only fails (inside catch) on network error */
 /* we want to combine all errors (also from the api) in a fetcher function */
-function fetcher()
-{
-    return new Promise((resolve, reject) => {
-        fetch('https://httpbin.org/html')
-            .then((result) => {
-                return result.json();
+function fetch(url, args) {
+    if (args.cache === undefined) {
+        args.cache = 'no-cache';
+    }
+    if (args.headers === undefined) {
+        args.headers = { 'Content-Type': 'application/json' };
+    }
+    return new Promise(resolve => {
+        fetch(url, args)
+            .then(response => {
+                let data = response.json(),
+                    status = response.status;
+                if (status == 200 || status == 304) {
+                    return data;
+                }
+                return { success: false, message: status };
             })
-            .catch((error) => {
+            .catch(error => {
                 return { success: false, message: error };
             })
-            .then((data) => {
-                if (data === undefined || data === '' || data === null || typeof data !== 'object' || data.success === false) {
-                    reject(data);
-                } else {
-                    resolve(data);
-                }
+            .then(response => {
+                resolve(response);
             });
     });
 }
 
-async function main()
-{
-   try {
-       await fetcher();
-       await fetcher();
-       await fetcher();
-   }
-   catch(e)
-   {
-       console.log('error');
-   }
+async function main() {
+    await fetch(url, args);
+    await fetch(url, args);
+    await fetch(url, args);
 }
 
 main();
