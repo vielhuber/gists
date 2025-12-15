5830c84b8f7c1e62ac5f607b505d8b400edeafae
David Vielhuber <david@vielhuber.de>
2025-12-16 00:08:54 +0100

    last update on 2025-12-16

--- gists/MySQL Importexport backuprestore dump mysqldump on command line #sql/README.sh
+++ gists/MySQL Importexport backuprestore dump mysqldump on command line #sql/README.sh
@@ -1,23 +1,23 @@
-// export
+# export
 mysqldump -h localhost --port 3306 -u username -p"password" --routines database > dump.sql
 
-// import
+# import
 mysql -h localhost --port 3306 -u username -p"XXX" --default-character-set=utf8 database < dump.sql
 
-// import with progress
+# import with progress
 pv dump.sql | mysql -h localhost --port 3306 -u username -p"XXX" --default-character-set=utf8 database
 
-// note when using passwords with "$": escape passwords needed
+# note when using passwords with "$": escape passwords needed
 mysql -p "ugly\$password"
 
-// on windows, these tools can be found in
+# on windows, these tools can be found in
 cd C:\Program Files\MySQL\MySQL Server 5.6\bin
 
-// options of mysql
+# options of mysql
 --force: force import (ignore/skip errors)
 --default-character-set: always choose "utf8mb4"
 
-// options of mysqldump
+# options of mysqldump
 --no-tablespaces: don't include tablespaces
 --skip-add-locks: don't surround statements with locks
 --skip-add-drop-table: don't add drop table statements
@@ -37,32 +37,32 @@ cd C:\Program Files\MySQL\MySQL Server 5.6\bin
 --skip-triggers: don't include triggers
 --default-character-set: always choose "utf8mb4"
 
-// exclude data from specific table (but get schema)
-  // option 1 (preferred)
+# exclude data from specific table (but get schema)
+  # option 1 (preferred)
   mysqldump --no-data --skip-triggers dbname > dump.sql
   mysqldump --no-create-info --ignore-table=dbname.tblname dbname >> dump.sql
   // option 2 (not good)
   mysqldump --ignore-table=dbname.tblname dbname > dump.sql
   mysqldump --no-data dbname tblname >> dump.sql
 
-// restore single database from big file dumped with --all-databases
-// download https://github.com/kedarvj/mysqldumpsplitter
+# restore single database from big file dumped with --all-databases
+# download https://github.com/kedarvj/mysqldumpsplitter
 sh mysqldumpsplitter.sh --source backup.sql --extract DB --match_str database-name
 
-// suppress warning "Warning: Using a password on the command line interface can be insecure."
+# suppress warning "Warning: Using a password on the command line interface can be insecure."
 file_put_contents('my.cnf','[client]'.PHP_EOL.'user = root'.PHP_EOL.'password = root'.PHP_EOL.'host = localhost');
 exec('mysqldump --defaults-extra-file=myo.cnf --port 3306 database > dump.sql');
 exec('mysql --defaults-extra-file=myo.cnf --default-character-set=utf8 database < dump.sql');
 unlink('my.cnf');
 
-// since mysql 5.7/8.0.21 accessing the information_schema.files table now requires the process privilege.
-// most providers don't have this option available. This results in the error message:
-// Error: 'Access denied; you need (at least one of) the PROCESS privilege(s) for this operation' when trying to dump tablespaces
-// to prevent that, add
+# since mysql 5.7/8.0.21 accessing the information_schema.files table now requires the process privilege.
+# most providers don't have this option available. This results in the error message:
+# Error: 'Access denied; you need (at least one of) the PROCESS privilege(s) for this operation' when trying to dump tablespaces
+# to prevent that, add
 --no-tablespaces
 
-// if errors with "NO_AUTO_CREATE_USER" appears, simply replace all occurences of that string with "" and reimport
+# if errors with "NO_AUTO_CREATE_USER" appears, simply replace all occurences of that string with "" and reimport
 
-// speed up even more
+# speed up even more
 https://serverfault.com/a/568465/442998
 https://dba.stackexchange.com/a/83385/188169
