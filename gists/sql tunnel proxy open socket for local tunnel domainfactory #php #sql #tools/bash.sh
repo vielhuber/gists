b3b5d0e0d8c79b2297b7487e1414e6830c88f618
David Vielhuber <david@vielhuber.de>
2026-01-16 00:07:36 +0100

    last update on 2026-01-16

--- gists/sql tunnel proxy open socket for local tunnel domainfactory #php #sql #tools/bash.sh
+++ gists/sql tunnel proxy open socket for local tunnel domainfactory #php #sql #tools/bash.sh
@@ -1,1 +1,7 @@
-ssh -o UserKnownHostsFile=/dev/null -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -TNL 5001:/var/lib/mysql/mysql.sock ssh-username@host # then connect via localhost:5001 to mysql db
+# mysql via unix-socket (local 5001 goes to remote socket)
+ssh -o UserKnownHostsFile=/dev/null -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -l ssh-username -TNL 5001:/var/lib/mysql/mysql.sock host.tld
+
+# mysql via tunnel (local 5001 goes to remote 3306)
+ssh -o UserKnownHostsFile=/dev/null -o TCPKeepAlive=yes -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -l ssh-username -TNL 5001:127.0.0.1:3306 host.tld
+
+# then connect via localhost:5001 to mysql db
