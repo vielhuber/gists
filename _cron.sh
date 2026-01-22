c3fd977057b0994256bb006537974e869fba83f1
David Vielhuber <david@vielhuber.de>
2026-01-22 17:12:45 +0100

    Generalize bash script.

--- _cron.sh
+++ _cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /kunden/362380_80937/gists
+cd "$(dirname "$0")"
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git pull
 env -i /usr/bin/php -f _sync.php
 git add -A .
