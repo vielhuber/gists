b232ce6be4f1370f9dff3d716ec33cd1efe0f996
David Vielhuber <david@vielhuber.de>
2026-05-14 04:09:11 +0200

    last update on 2026-05-14

--- gists/cookies #php/index.php
+++ gists/cookies #php/index.php
@@ -4,7 +4,7 @@ unset($_COOKIE['cookie']); setcookie('cookie', '', time() - 3600, '/');
 
 // set
 setcookie('cookie', $value, time()+60*60*24*1, '/');
-if (headers_sent()) { setcookie('cookie', $value, time()+60*60*24*1, '/'); } // prevent "headers already sent" error
+if(!(headers_sent() || ob_get_length() > 0)) { setcookie('cookie', $value, time()+60*60*24*1, '/'); } // always check first, this prevents "headers already sent" error
 $_COOKIE['cookie'] = $value; // immediately set it for current request
 
 // get
